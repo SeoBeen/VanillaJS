f9c627d759f2765c4e3ae87909da50422538c133
SeoBeen <jya3392@naver.com>
2020-08-07 13:22:28 +0900

    add index js
    function

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-a = 221;
-b = a - 5;
-console.log(b);
+/* String
+const what = "seobeen";
+*/
+/*
+Boolean
+const wat = true;
+*/
+/*
+Number
+const wat = 666;
+*/
+/* Float
+const what = 55.1;
+*/
+/*
+//camel case로 변수명 선언
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
+
+const beenInfo = {
+  name: "Seobeen",
+  age: 26,
+  gender: "Male",
+  isHandsome: true,
+  favMovies: ["Along the gods", "LOTR"],
+  favFood: [
+    { name: "Kimchi", fatty: false },
+    {
+      name: "Cheese burger",
+      fattry: true,
+    },
+  ],
+};
+
+console.log(beenInfo.gender);
+
+beenInfo.gender = "Female";
+console.log(beenInfo.gender);
+console.log(beenInfo);
+
+*/
+function sayHello() {}
